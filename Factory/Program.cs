22246ca3ecb29c093ed0cf67661a579cded64f06
a0987143311 <a0987143311@gmail.com>
2022-08-28 21:11:13 +0800

    Add project files.

---
+++ Factory/Program.cs
@@ -0,0 +1,71 @@
+﻿Product product = new Product() { Name = "C300" };
+IProductInspectorFactory factory = new CPUProdcutInspectorFactory();
+var service = new InspectionService(factory);
+
+service.Inspect(product);
+
+class InspectionService
+{
+    private readonly IProductInspectorFactory _productInspectorFactory;
+    public InspectionService(
+        IProductInspectorFactory productInspectorFactory)
+    {
+        _productInspectorFactory = productInspectorFactory;
+    }
+    public void Inspect(Product product)
+    {
+       
+        var result = _productInspectorFactory.CreateInpector().Inspect(product);
+        Console.WriteLine("save result to db...");
+        Console.WriteLine(result);
+    }    
+}
+
+
+
+
+class Product
+{
+    public string Name { get; set; }
+}
+
+interface IProductInspector
+{
+    string Inspect(Product product);
+}
+
+class CPUProductInspector : IProductInspector
+{
+    public string Inspect(Product product)
+    {
+        return $"product name: {product.Name}, result by cpu";
+    }
+}
+class GPUProductInspector : IProductInspector
+{
+    public string Inspect(Product product)
+    {
+        return $"product name: {product.Name}, result by gpu";
+    }
+}
+
+interface IProductInspectorFactory
+{
+    IProductInspector CreateInpector();
+}
+
+class CPUProdcutInspectorFactory : IProductInspectorFactory
+{
+    public IProductInspector CreateInpector()
+    {
+        return new CPUProductInspector();
+    }
+}
+class GPUProdcutInspectorFactory : IProductInspectorFactory
+{
+    public IProductInspector CreateInpector()
+    {
+        return new GPUProductInspector();
+    }
+}
+
